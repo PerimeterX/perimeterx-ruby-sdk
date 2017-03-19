4ca47d47bf5baadfb2da70137fb52e5c270c2930
nitzanpx <nitzan@perimeterx.com>
2017-03-19 17:02:39 +0200

    50% px_ctx ready

--- lib/perimeter_x.rb
+++ lib/perimeter_x.rb
@@ -6,26 +6,26 @@ require 'perimeterx/internal/cookie_validator'
 require 'perimeterx/internal/s2s_validator'
 
 module PerimeterX
-
   class PxModule
-
     L = PxLogger.instance
 
     attr_accessor :px_config
     attr_accessor :px_client
     attr_accessor :instance
 
-    def initialize(params = Hash.new())
-      @px_config = Configuration.new(params)
+    def initialize(params)
+      @px_config = Configuration.new(params).configuration
     end
 
-    def pxVerify
+    def pxVerify(env)
       L.info("pxVerify started")
-      if !px_config.enable_module
+      req = ActionDispatch::Request.new(env)
+
+      if !px_config[:module_enabled]
         L.warn("Module is disabled")
         return
       end
-      px_ctx = PerimeterXContext.new(px_config)
+      px_ctx = PerimeterXContext.new(px_config, req)
 
       captcha_validator = PerimeterxCaptchaValidator.new(px_ctx, px_config)
       if (captcha_validator.verify())
--- lib/perimeterx/internal/perimeter_x_context.rb
+++ lib/perimeterx/internal/perimeter_x_context.rb
@@ -3,9 +3,56 @@ require 'perimeterx/utils/px_logger'
 class PerimeterXContext
   L = PxLogger.instance
 
-  def initialize(px_config)
+  attr_accessor :px_cookies
+  attr_accessor :px_captcha
+  attr_accessor :start_time
+  attr_accessor :headers
+  attr_accessor :px_config
+  attr_accessor :hostname
+  attr_accessor :user_agent
+  attr_accessor :uri
+  attr_accessor :full_url
+
+  def initialize(px_config, req)
     L.info("PerimeterXContext: initialize")
+
+    @px_config = px_config
+    @px_cookies = Hash.new
+    @headers = Hash.new
+    cookies = req.cookies
+    if (!cookies.empty?)
+      # Prepare hashed cookies
+      cookies.each do |k,v|
+        case k
+          when"_px3"
+            @px_cookies[:v3] = v
+          when "_px"
+            @px_cookies[:v1] = v
+          when "_pxCaptcha"
+            @px_captcha = v
+        end
+      end #end case
+    end#end empty cookies
+
+    @start_time = Time.now
+    req.headers.each do |k,v|
+      if(k.start_with? "HTTP_")
+        header = k.gsub("_","-").downcase
+        @headers[header] = v
+      end
+    end#end headers foreach
+
+    @hostname = req.headers['HTTP_HOST']
+    @user_agent = req.headers['HTTP_USER_AGENT'] ? req.headers['HTTP_USER_AGENT'] : ''
+    @uri = px_config.custom_uri ? px_config.custom_uri : req.headers['REQUEST_URI']
+    @full_url = self_url(req)
+
+  end #end init
+
+  def self_url(req)
 
   end
 
-end
+  private :self_url
+
+end #end class
