62d4bf410087eddf42a38d928a7a6a1f73804b5b
nitzanpx <nitzan@perimeterx.com>
2017-04-02 17:31:20 +0300

    replaced context build with req builtin methods

--- lib/perimeterx/internal/perimeter_x_context.rb
+++ lib/perimeterx/internal/perimeter_x_context.rb
@@ -16,7 +16,7 @@ module PxModule
       cookies = req.cookies
       if (!cookies.empty?)
         # Prepare hashed cookies
-        cookies.each do |k,v|
+        cookies.each do |k, v|
           case k.to_s
             when "_px3"
               @context[:px_cookie][:v3] = v
@@ -26,37 +26,37 @@ module PxModule
               @context[:px_captcha] = v
           end
         end #end case
-      end#end empty cookies
+      end #end empty cookies
 
-      req.headers.each do |k,v|
-        if(k.start_with? "HTTP_")
-          header = k.to_s.gsub("HTTP_","")
-          header = header.gsub("_","-").downcase
+      req.headers.each do |k, v|
+        if (k.start_with? "HTTP_")
+          header = k.to_s.gsub("HTTP_", "")
+          header = header.gsub("_", "-").downcase
           @context[:headers][header.to_sym] = v
         end
-      end#end headers foreach
+      end #end headers foreach
 
-      @context[:hostname]= req.headers['HTTP_HOST']
-      @context[:user_agent] = req.headers['HTTP_USER_AGENT'] ? req.headers['HTTP_USER_AGENT'] : ''
+      @context[:hostname]= req.server_name
+      @context[:user_agent] = req.user_agent ? req.user_agent : ''
       @context[:uri] = px_config[:custom_uri] ? px_config[:custom_uri].call(req)  : req.headers['REQUEST_URI']
-      @context[:full_url] = self_url(req)
+      @context[:full_url] = req.original_url
       @context[:score] = 0
 
       if px_config.key?(:custom_user_ip)
-        @context[:ip] = px_config[:custom_user_ip]
+        @context[:ip] = req.headers[px_config[:custom_user_ip]]
       elsif px_config.key?(:px_custom_user_ip_method)
         @context[:ip] = px_config[:px_custom_user_ip_method].call(req)
       else
-        @context[:ip] = req.headers['REMOTE_ADDR'];
+        @context[:ip] = req.ip
       end
 
-      if req.headers['SERVER_PROTOCOL']
-          httpVer = req.headers['SERVER_PROTOCOL'].split("/")
+      if req.server_protocol
+          httpVer = req.server_protocol.split("/")
           if httpVer.size > 0
               @context[:http_version] = httpVer[1];
           end
       end
-      @context[:http_method] = req.headers['REQUEST_METHOD'];
+      @context[:http_method] = req.method
 
     end #end init
 
@@ -77,14 +77,5 @@ module PxModule
       return @context[:px_cookie].key?(:v3) ? @context[:px_cookie][:v3] : @context[:px_cookie][:v1]
     end
 
-    def self_url(req)
-      s = req.headers.key?('HTTPS') && req.headers['HTTPS'] == "on" ? "s" : "" #check if HTTPS or HTTP
-      l = req.headers['SERVER_PROTOCOL'].downcase #get protocol and downcase it
-      protocol = "#{l[0,l.index('/')]}#{s}#{l[(l.index('/') ),l.size]}" #concat http{s}:/x.y
-      port = (req.headers["SERVER_PORT"] != "80") ? ":#{req.headers["SERVER_PORT"]}" : ""
-      return "#{l}://#{req.headers['HTTP_HOST']}#{@uri}" #concant str
-    end
-
-    private :self_url
   end
 end
