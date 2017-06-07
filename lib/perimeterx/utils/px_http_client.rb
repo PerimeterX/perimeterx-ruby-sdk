1d20862a5d1c960d2498edff418d774715add582
nitzanpx <nitzan@perimeterx.com>
2017-06-07 12:14:40 +0300

    updated pr comments

--- lib/perimeterx/utils/px_http_client.rb
+++ lib/perimeterx/utils/px_http_client.rb
@@ -15,7 +15,15 @@ module PxModule
       @logger.debug("PxHttpClient[initialize]: HTTP client is being initilized with base_uri: #{px_config[:perimeterx_server_host]}")
     end
 
-    def post(path, body, headers, api_timeout = 1, timeout = 1)
+    # Runs a POST commant to Perimeter X servers
+    # Params:
+    # +path+:: string containing uri
+    # +body+:: hash object, containing the request body, must be converted to json format
+    # +headers+:: hash object, hold headers
+    # +api_timeout+:: int, sets the timeout for a request
+    # +connection_timeout+:: int, sets the timeout for opening a connection
+
+    def post(path, body, headers, api_timeout = 1, connection_timeout = 1)
       s = Time.now
       begin
         @logger.debug("PxHttpClient[post]: posting to #{path} headers {#{headers.to_json()}} body: {#{body.to_json()}} ")
@@ -23,7 +31,8 @@ module PxModule
             "#{px_config[:perimeterx_server_host]}#{path}",
             headers: headers,
             body: body.to_json,
-            timeout: api_timeout
+            timeout: api_timeout,
+            connecttimeout: connection_timeout
         )
         if response.timed_out?
           @logger.warn('PerimeterxS2SValidator[verify]: request timed out')
