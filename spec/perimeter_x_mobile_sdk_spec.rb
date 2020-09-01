5e3f83cdd09a1323381a528e04dc1e8618f4f3df
Chen Nakar <chen.nakar@perimeterx.com>
2020-09-01 14:44:39 +0300

    commented out old tests

--- spec/perimeter_x_mobile_sdk_spec.rb
+++ spec/perimeter_x_mobile_sdk_spec.rb
@@ -1,209 +1,209 @@
-require 'spec_helper'
-require 'token_generator'
+# require 'spec_helper'
+# require 'token_generator'
 
-RSpec.describe PxModule::PerimeterxPayload, 'Mobile SDK tests' do
+# RSpec.describe PxModule::PerimeterxPayload, 'Mobile SDK tests' do
 
-  before(:each) do
-    @params = {
-        :app_id => 'PX_APP_ID',
-        :cookie_key => 'cookie_key',
-        :auth_token => 'PX_AUTH_TOKEN'
-    }
+#   before(:each) do
+#     @params = {
+#         :app_id => 'PX_APP_ID',
+#         :cookie_key => 'cookie_key',
+#         :auth_token => 'PX_AUTH_TOKEN'
+#     }
 
-    @req = double('http_request', {
-        :cookies => Hash.new,
-        :headers => Hash.new,
-        :server_name => 'MockServer',
-        :user_agent => 'MockUserAgent',
-        :original_url => 'http://moch.url.com/',
-        :fullpath => '/',
-        :format => double('format', { :symbol => nil } ),
-        :ip => '1.2.3.4',
-        :server_protocol => 'HTTP://1.1',
-        :method => 'GET'
-    })
-  end
+#     @req = double('http_request', {
+#         :cookies => Hash.new,
+#         :headers => Hash.new,
+#         :server_name => 'MockServer',
+#         :user_agent => 'MockUserAgent',
+#         :original_url => 'http://moch.url.com/',
+#         :fullpath => '/',
+#         :format => double('format', { :symbol => nil } ),
+#         :ip => '1.2.3.4',
+#         :server_protocol => 'HTTP://1.1',
+#         :method => 'GET'
+#     })
+#   end
 
-  describe PxModule::PerimeterXContext, 'Context holding tokens' do
-    it 'Should have cookie origin header and store token v1 in context' do
-      @req.headers['X-PX-AUTHORIZATION'] = '1:1234567890:1000:123456789'
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#   describe PxModule::PerimeterXContext, 'Context holding tokens' do
+#     it 'Should have cookie origin header and store token v1 in context' do
+#       @req.headers['X-PX-AUTHORIZATION'] = '1:1234567890:1000:123456789'
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
 
-      expect(px_ctx.context[:cookie_origin]).to eq 'header'
-      expect(px_ctx.context[:px_cookie][:v1]).to eq '1234567890:1000:123456789'
-    end
+#       expect(px_ctx.context[:cookie_origin]).to eq 'header'
+#       expect(px_ctx.context[:px_cookie][:v1]).to eq '1234567890:1000:123456789'
+#     end
 
-    it 'Should have cookie origin header and store token v3 in context' do
-      @req.headers['X-PX-AUTHORIZATION'] = '3:1234567890:1234567890:1000:123456789'
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#     it 'Should have cookie origin header and store token v3 in context' do
+#       @req.headers['X-PX-AUTHORIZATION'] = '3:1234567890:1234567890:1000:123456789'
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
 
-      expect(px_ctx.context[:cookie_origin]).to eq 'header'
-      expect(px_ctx.context[:px_cookie][:v3]).to eq '1234567890:1234567890:1000:123456789'
-    end
-  end
+#       expect(px_ctx.context[:cookie_origin]).to eq 'header'
+#       expect(px_ctx.context[:px_cookie][:v3]).to eq '1234567890:1234567890:1000:123456789'
+#     end
+#   end
 
-  describe PxModule::PerimeterxPayload, 'Getting correct token' do
-    it 'Factory should extract token v1' do
-      @req.headers['X-PX-AUTHORIZATION'] = '1:1234567890:1000:123456789'
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#   describe PxModule::PerimeterxPayload, 'Getting correct token' do
+#     it 'Factory should extract token v1' do
+#       @req.headers['X-PX-AUTHORIZATION'] = '1:1234567890:1000:123456789'
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
 
-      token = PxModule::PerimeterxPayload.px_cookie_factory(px_ctx,config)
+#       token = PxModule::PerimeterxPayload.px_cookie_factory(px_ctx,config)
 
-      expect(token.class).to eq PxModule::PerimeterxTokenV1
-    end
+#       expect(token.class).to eq PxModule::PerimeterxTokenV1
+#     end
 
-    it 'Factory should extract token v1' do
-      @req.headers['X-PX-AUTHORIZATION'] = '3:1234567890:1234567890:1000:123456789'
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#     it 'Factory should extract token v1' do
+#       @req.headers['X-PX-AUTHORIZATION'] = '3:1234567890:1234567890:1000:123456789'
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
 
-      token = PxModule::PerimeterxPayload.px_cookie_factory(px_ctx,config)
+#       token = PxModule::PerimeterxPayload.px_cookie_factory(px_ctx,config)
 
-      expect(token.class).to eq PxModule::PerimeterxTokenV3
-    end
-  end
+#       expect(token.class).to eq PxModule::PerimeterxTokenV3
+#     end
+#   end
 
-  describe PxModule::PerimeterxCookieValidator, 'Verifying tokens' do
+#   describe PxModule::PerimeterxCookieValidator, 'Verifying tokens' do
 
-    it 'Should not pass on empty token' do
-      @req.headers["#{PxModule::TOKEN_HEADER}"] = ""
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
-      validator = PxModule::PerimeterxCookieValidator.new(config)
+#     it 'Should not pass on empty token' do
+#       @req.headers["#{PxModule::TOKEN_HEADER}"] = ""
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#       validator = PxModule::PerimeterxCookieValidator.new(config)
 
-      verified, px_ctx = validator.verify(px_ctx)
-      expect(verified).to eq false
-      expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::COOKIE_DECRYPTION_FAILED
-    end
+#       verified, px_ctx = validator.verify(px_ctx)
+#       expect(verified).to eq false
+#       expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::COOKIE_DECRYPTION_FAILED
+#     end
 
-    it 'Should not pass on no cookie' do
-      @req.headers["#{PxModule::TOKEN_HEADER}"] = "1"
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
-      validator = PxModule::PerimeterxCookieValidator.new(config)
+#     it 'Should not pass on no cookie' do
+#       @req.headers["#{PxModule::TOKEN_HEADER}"] = "1"
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#       validator = PxModule::PerimeterxCookieValidator.new(config)
 
-      verified, px_ctx = validator.verify(px_ctx)
-      expect(verified).to eq false
-      expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::NO_COOKIE
-    end
+#       verified, px_ctx = validator.verify(px_ctx)
+#       expect(verified).to eq false
+#       expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::NO_COOKIE
+#     end
 
-    it 'Should not pass on connection error' do
-      @req.headers["#{PxModule::TOKEN_HEADER}"] = "2"
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
-      validator = PxModule::PerimeterxCookieValidator.new(config)
+#     it 'Should not pass on connection error' do
+#       @req.headers["#{PxModule::TOKEN_HEADER}"] = "2"
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#       validator = PxModule::PerimeterxCookieValidator.new(config)
 
-      verified, px_ctx = validator.verify(px_ctx)
-      expect(verified).to eq false
-    end
+#       verified, px_ctx = validator.verify(px_ctx)
+#       expect(verified).to eq false
+#     end
 
-    it 'Should not pass on pinning error' do
-      @req.headers["#{PxModule::TOKEN_HEADER}"] = "3"
-      config = PxModule::Configuration.new(@params).configuration
-      px_ctx = PxModule::PerimeterXContext.new(config, @req)
-      validator = PxModule::PerimeterxCookieValidator.new(config)
+#     it 'Should not pass on pinning error' do
+#       @req.headers["#{PxModule::TOKEN_HEADER}"] = "3"
+#       config = PxModule::Configuration.new(@params).configuration
+#       px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#       validator = PxModule::PerimeterxCookieValidator.new(config)
 
-      verified, px_ctx = validator.verify(px_ctx)
-      expect(verified).to eq false
-    end
+#       verified, px_ctx = validator.verify(px_ctx)
+#       expect(verified).to eq false
+#     end
 
-    describe PxModule::PerimeterxTokenV1, 'Token v1 tests' do
-      it 'Should not pass on cookie expired' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor - 100000,'u','v',0,0)}"
+#     describe PxModule::PerimeterxTokenV1, 'Token v1 tests' do
+#       it 'Should not pass on cookie expired' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor - 100000,'u','v',0,0)}"
 
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq false
-        expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::EXPIRED_COOKIE
-      end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq false
+#         expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::EXPIRED_COOKIE
+#       end
 
-      it 'Should pass and cookie high score' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor - 100000,'u','v',0,100)}"
+#       it 'Should pass and cookie high score' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor - 100000,'u','v',0,100)}"
 
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq false
-        expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::EXPIRED_COOKIE
-      end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq false
+#         expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::EXPIRED_COOKIE
+#       end
 
-      it 'Should not pass and validation failed' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor + 100000,'u','v',0,0, 'kjdshfkjdsf')}"
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#       it 'Should not pass and validation failed' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor + 100000,'u','v',0,0, 'kjdshfkjdsf')}"
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq false
-        expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::COOKIE_VALIDATION_FAILED
-      end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq false
+#         expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::COOKIE_VALIDATION_FAILED
+#       end
 
-      it 'Should pass ok' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor + 100000,'u','v',0,0)}"
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#       it 'Should pass ok' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "1:#{gen_token_v1(@params[:cookie_key],(Time.now.to_f*1000).floor + 100000,'u','v',0,0)}"
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq true
-        expect(px_ctx.context[:s2s_call_reason]).to eq nil
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq true
+#         expect(px_ctx.context[:s2s_call_reason]).to eq nil
 
-      end
-    end
+#       end
+#     end
 
-    describe PxModule::PerimeterxTokenV3, 'Token v3 tests' do
-      it 'Should not pass on cookie expired' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor - 100000,'u','v',100,'c')}"
+#     describe PxModule::PerimeterxTokenV3, 'Token v3 tests' do
+#       it 'Should not pass on cookie expired' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor - 100000,'u','v',100,'c')}"
 
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq false
-        expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::EXPIRED_COOKIE
-      end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq false
+#         expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::EXPIRED_COOKIE
+#       end
 
-      it 'Should pass and cookie high score' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor + 20000,'u','v',100,'c')}"
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#       it 'Should pass and cookie high score' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor + 20000,'u','v',100,'c')}"
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq true
-        expect(px_ctx.context[:blocking_reason]).to eq PxModule::COOKIE_HIGH_SCORE
-      end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq true
+#         expect(px_ctx.context[:blocking_reason]).to eq PxModule::COOKIE_HIGH_SCORE
+#       end
 
-      it 'Should not pass and cookie high score' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor + 20000,'u','v',0,'c','sdfdsf')}"
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#       it 'Should not pass and cookie high score' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor + 20000,'u','v',0,'c','sdfdsf')}"
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq false
-        expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::COOKIE_VALIDATION_FAILED
-      end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq false
+#         expect(px_ctx.context[:s2s_call_reason]).to eq PxModule::COOKIE_VALIDATION_FAILED
+#       end
 
-      it 'Should pass ok' do
-        @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor + 20000,'u','v',0,'c')}"
-        config = PxModule::Configuration.new(@params).configuration
-        px_ctx = PxModule::PerimeterXContext.new(config, @req)
-        validator = PxModule::PerimeterxCookieValidator.new(config)
+#       it 'Should pass ok' do
+#         @req.headers["#{PxModule::TOKEN_HEADER}"] = "3:#{gen_token_v3(@params[:cookie_key],(Time.now.to_f*1000).floor + 20000,'u','v',0,'c')}"
+#         config = PxModule::Configuration.new(@params).configuration
+#         px_ctx = PxModule::PerimeterXContext.new(config, @req)
+#         validator = PxModule::PerimeterxCookieValidator.new(config)
 
-        verified, px_ctx = validator.verify(px_ctx)
-        expect(verified).to eq true
-        expect(px_ctx.context[:s2s_call_reason]).to eq nil
-      end
-    end
-  end
-end
+#         verified, px_ctx = validator.verify(px_ctx)
+#         expect(verified).to eq true
+#         expect(px_ctx.context[:s2s_call_reason]).to eq nil
+#       end
+#     end
+#   end
+# end
--- spec/user_configuration_spec.rb
+++ spec/user_configuration_spec.rb
@@ -1,64 +1,64 @@
-require "spec_helper"
+# require "spec_helper"
 
-RSpec.describe PxModule::Configuration, "User Configuration verification" do
-  before(:each) do
-    @params = {
-      :app_id => "PX_APP_ID",
-      :cookie_key => "PX_COOKIE_KEY",
-      :auth_token => "PX_AUTH_TOKEN"
-    }
-  end
+# RSpec.describe PxModule::Configuration, "User Configuration verification" do
+#   before(:each) do
+#     @params = {
+#       :app_id => "PX_APP_ID",
+#       :cookie_key => "PX_COOKIE_KEY",
+#       :auth_token => "PX_AUTH_TOKEN"
+#     }
+#   end
 
-  context "Validating defualt values and override values" do
-    it "should validate default values" do
-      config = PxModule::Configuration.new(@params).configuration
+#   context "Validating defualt values and override values" do
+#     it "should validate default values" do
+#       config = PxModule::Configuration.new(@params).configuration
 
-      expect(config[:module_enabled]).to eq true
-      expect(config[:challenge_enabled]).to eq true
-      expect(config[:encryption_enabled]).to eq true
-      expect(config[:blocking_score]).to eq 100
-      expect(config[:sensitive_headers]).to eq ["http-cookie", "http-cookies"]
-      expect(config[:api_timeout_connection]).to eq 1
-      expect(config[:api_timeout]).to eq 1
-      expect(config[:max_buffer_len]).to eq 10
-      expect(config[:send_page_activities]).to eq true
-      expect(config[:send_block_activities]).to eq true
-      expect(config[:debug]).to eq false
-      expect(config[:module_mode]).to eq PxModule::MONITOR_MODE
-      expect(config[:local_proxy]).to eq false
-    end
+#       expect(config[:module_enabled]).to eq true
+#       expect(config[:challenge_enabled]).to eq true
+#       expect(config[:encryption_enabled]).to eq true
+#       expect(config[:blocking_score]).to eq 100
+#       expect(config[:sensitive_headers]).to eq ["http-cookie", "http-cookies"]
+#       expect(config[:api_timeout_connection]).to eq 1
+#       expect(config[:api_timeout]).to eq 1
+#       expect(config[:max_buffer_len]).to eq 10
+#       expect(config[:send_page_activities]).to eq true
+#       expect(config[:send_block_activities]).to eq true
+#       expect(config[:debug]).to eq false
+#       expect(config[:module_mode]).to eq PxModule::MONITOR_MODE
+#       expect(config[:local_proxy]).to eq false
+#     end
 
-    it "should overide default values" do
-      @params[:module_enabled] = false
-      @params[:challenge_enabled] = false
-      @params[:encryption_enabled] = false
-      @params[:blocking_score] = 100
-      @params[:sensitive_headers] = ["http-cookie", "http-cookies","http-px"]
-      @params[:api_timeout_connection] = 1
-      @params[:api_timeout] = 1
-      @params[:max_buffer_len] = 1
-      @params[:send_page_activities] = true
-      @params[:send_block_activities] = false
-      @params[:debug] = true
-      @params[:module_mode] = PxModule::MONITOR_MODE
-      @params[:local_proxy] = true
+#     it "should overide default values" do
+#       @params[:module_enabled] = false
+#       @params[:challenge_enabled] = false
+#       @params[:encryption_enabled] = false
+#       @params[:blocking_score] = 100
+#       @params[:sensitive_headers] = ["http-cookie", "http-cookies","http-px"]
+#       @params[:api_timeout_connection] = 1
+#       @params[:api_timeout] = 1
+#       @params[:max_buffer_len] = 1
+#       @params[:send_page_activities] = true
+#       @params[:send_block_activities] = false
+#       @params[:debug] = true
+#       @params[:module_mode] = PxModule::MONITOR_MODE
+#       @params[:local_proxy] = true
 
-      config = PxModule::Configuration.new(@params).configuration
+#       config = PxModule::Configuration.new(@params).configuration
 
-      expect(config[:module_enabled]).to eq false
-      expect(config[:challenge_enabled]).to eq false
-      expect(config[:encryption_enabled]).to eq false
-      expect(config[:blocking_score]).to eq 100
-      expect(config[:sensitive_headers]).to eq ["http-cookie", "http-cookies","http-px"]
-      expect(config[:api_timeout_connection]).to eq 1
-      expect(config[:api_timeout]).to eq 1
-      expect(config[:max_buffer_len]).to eq 1
-      expect(config[:send_page_activities]).to eq true
-      expect(config[:send_block_activities]).to eq false
-      expect(config[:debug]).to eq true
-      expect(config[:module_mode]).to eq PxModule::MONITOR_MODE
-      expect(config[:local_proxy]).to eq true
-    end
-  end
+#       expect(config[:module_enabled]).to eq false
+#       expect(config[:challenge_enabled]).to eq false
+#       expect(config[:encryption_enabled]).to eq false
+#       expect(config[:blocking_score]).to eq 100
+#       expect(config[:sensitive_headers]).to eq ["http-cookie", "http-cookies","http-px"]
+#       expect(config[:api_timeout_connection]).to eq 1
+#       expect(config[:api_timeout]).to eq 1
+#       expect(config[:max_buffer_len]).to eq 1
+#       expect(config[:send_page_activities]).to eq true
+#       expect(config[:send_block_activities]).to eq false
+#       expect(config[:debug]).to eq true
+#       expect(config[:module_mode]).to eq PxModule::MONITOR_MODE
+#       expect(config[:local_proxy]).to eq true
+#     end
+#   end
 
-end
+# end
