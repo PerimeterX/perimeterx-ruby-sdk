6f9eb5811e80d07b545e1b5163ab4c11702b2703
Ori <ori.gold@perimeterx.com>
2022-09-07 11:36:27 -0700

    s2s validator spec tests pass (using rspec mocks and not mocha mocks)

--- spec/perimeter_x_s2s_validator_spec.rb
+++ spec/perimeter_x_s2s_validator_spec.rb
@@ -1,99 +1,98 @@
-# require "spec_helper"
+require "spec_helper"
 
 
-# RSpec.describe PxModule::PerimeterxS2SValidator, "S2S Validator Tests" do
-#   before(:each) do
-#     @params = {
-#       :app_id => "PX_APP_ID",
-#       :cookie_key => "PX_COOKIE_KEY",
-#       :auth_token => "PX_AUTH_TOKEN"
-#     }
-#     @http_client = double("http_client")
+RSpec.describe PxModule::PerimeterxS2SValidator, "S2S Validator Tests" do
+  before(:each) do
+    @params = {
+      :app_id => "PX_APP_ID",
+      :cookie_key => "PX_COOKIE_KEY",
+      :auth_token => "PX_AUTH_TOKEN"
+    }
 
-#     @req = double("http_request", {
-#       :cookies => Hash.new,
-#       :headers => Hash.new,
-#       :server_name => "MockServer",
-#       :user_agent => "MockUserAgent",
-#       :original_url => "http://moch.url.com/",
-#       :fullpath => '/',
-#       :format => double("format", { :symbol => nil } ),
-#       :ip => "1.2.3.4",
-#       :server_protocol => "HTTP://1.1",
-#       :method => "GET"
-#       })
-#   end
+    @req = double("http_request", {
+      :cookies => Hash.new,
+      :headers => Hash.new,
+      :server_name => "MockServer",
+      :user_agent => "MockUserAgent",
+      :original_url => "http://moch.url.com/",
+      :fullpath => '/',
+      :format => double("format", { :symbol => nil } ),
+      :ip => "1.2.3.4",
+      :server_protocol => "HTTP://1.1",
+      :method => "GET"
+    })
 
-
-#   # it "pass request because sending failed" do
-#   #   @http_client.expects(:post).returns(nil)
-#   #   config = PxModule::Configuration.new(@params).configuration;
-#   #   px_ctx = PxModule::PerimeterXContext.new(config, @req)
-#   #   validator = PxModule::PerimeterxS2SValidator.new(config, @http_client)
-
-#   #   px_ctx = validator.verify(px_ctx)
-#   #   expect(px_ctx).to eq px_ctx
-#   # end
-
-#   # it "ctx will contain block_action from captcha type" do
-#   #   @http_client.expects(:post).returns( double("response", {:code => 200 ,:body  => "{ score: 69, uuid: 'uuid', action: 'c' }"}) )
-#   #   config = PxModule::Configuration.new(@params).configuration;
-#   #   px_ctx = PxModule::PerimeterXContext.new(config, @req)
-#   #   validator = PxModule::PerimeterxS2SValidator.new(config, @http_client)
-#   #   expect(px_ctx.context[:block_action]).to be_nil
-
-#   #   px_ctx = validator.verify(px_ctx)
-#   #   expect(px_ctx.context[:block_action]).to eq "captcha"
-#   # end
-
-#   # it "ctx will contain block_action from block type" do
-#   #   @http_client.expects(:post).returns( double("response", {:code => 200 ,:body  => "{ score: 69, uuid: 'uuid', action: 'b' }"}) )
-#   #   config = PxModule::Configuration.new(@params).configuration;
-#   #   px_ctx = PxModule::PerimeterXContext.new(config, @req)
-#   #   validator = PxModule::PerimeterxS2SValidator.new(config, @http_client)
-#   #   expect(px_ctx.context[:block_action]).to be_nil
-
-#   #   px_ctx = validator.verify(px_ctx)
-#   #   expect(px_ctx.context[:block_action]).to eq "block"
-#   #   expect(px_ctx.context[:blocking_reason]).to be_nil
-#   # end
-
-#   # it "ctx will contain block_action from block type and blocking_reason should s2s_high_score" do
-#   #   @http_client.expects(:post).returns( double("response", {:code => 200 ,:body  => "{ score: 71, uuid: 'uuid', action: 'b' }"}) )
-#   #   config = PxModule::Configuration.new(@params).configuration;
-#   #   px_ctx = PxModule::PerimeterXContext.new(config, @req)
-#   #   validator = PxModule::PerimeterxS2SValidator.new(config, @http_client)
-#   #   expect(px_ctx.context[:block_action]).to be_nil
-
-#   #   px_ctx = validator.verify(px_ctx)
-#   #   expect(px_ctx.context[:block_action]).to eq "block"
-#   #   expect(px_ctx.context[:blocking_reason]).to eq "s2s_high_score"
-#   # end
-
-#   # it "ctx will contain block_action from block type and blocking_reason should be empty" do
-#   #   @http_client.expects(:post).returns( double("response", {:code => 200 ,:body  => "{ score: 69, uuid: 'uuid', action: 'b' }"}) )
-#   #   config = PxModule::Configuration.new(@params).configuration;
-#   #   px_ctx = PxModule::PerimeterXContext.new(config, @req)
-#   #   validator = PxModule::PerimeterxS2SValidator.new(config, @http_client)
-#   #   expect(px_ctx.context[:block_action]).to be_nil
-
-#   #   px_ctx = validator.verify(px_ctx)
-#   #   expect(px_ctx.context[:block_action]).to eq "block"
-#   #   expect(px_ctx.context[:blocking_reason]).to be_nil
-#   # end
-
-#   # it "ctx will contain block_action from challange type and have block block_action_data" do
-#   #   @http_client.expects(:post).returns( double("response", {:code => 200 ,:body  => "{ score: 69, uuid: 'uuid', action: 'j', action_data: { body: 'challange_body' } }" } ) )
-#   #   config = PxModule::Configuration.new(@params).configuration;
-#   #   px_ctx = PxModule::PerimeterXContext.new(config, @req)
-#   #   validator = PxModule::PerimeterxS2SValidator.new(config, @http_client)
-#   #   expect(px_ctx.context[:block_action]).to be_nil
-
-#   #   px_ctx = validator.verify(px_ctx)
-#   #   expect(px_ctx.context[:block_action]).to eq 'challenge'
-#   #   expect(px_ctx.context[:block_action_data]).not_to be_nil
-#   # end
+    @config = PxModule::Configuration.new(@params).configuration;
+  end
 
 
+  it "pass request because sending failed" do
+    px_ctx = PxModule::PerimeterXContext.new(@config, @req)
 
-# end
+    http_client = double("http_client", :post => nil)
+    validator = PxModule::PerimeterxS2SValidator.new(@config, http_client)
+
+    px_ctx = validator.verify(px_ctx)
+    expect(px_ctx).to eq px_ctx
+  end
+
+  it "ctx will contain block_action from captcha type" do
+    px_ctx = PxModule::PerimeterXContext.new(@config, @req)
+
+    http_client = double("http_client", :post => double("response", {:code => 200 ,:body => '{ "score": 0, "uuid": "uuid", "action": "c", "status": 0 }'}))
+    validator = PxModule::PerimeterxS2SValidator.new(@config, http_client)
+    expect(px_ctx.context[:block_action]).to be_nil
+
+    px_ctx = validator.verify(px_ctx)
+    expect(px_ctx.context[:block_action]).to eq "captcha"
+  end
+
+  it "ctx will contain block_action from block type" do
+    px_ctx = PxModule::PerimeterXContext.new(@config, @req)
+
+    http_client = double("http_client", :post => double("response", {:code => 200, :body => '{ "score": 0, "uuid": "uuid", "action": "b", "status": 0 }'}))
+    validator = PxModule::PerimeterxS2SValidator.new(@config, http_client)
+    expect(px_ctx.context[:block_action]).to be_nil
+
+    px_ctx = validator.verify(px_ctx)
+    expect(px_ctx.context[:block_action]).to eq "block"
+    expect(px_ctx.context[:blocking_reason]).to be_nil
+  end
+
+  it "ctx will contain block_action from block type and blocking_reason should s2s_high_score" do
+    px_ctx = PxModule::PerimeterXContext.new(@config, @req)
+
+    http_client = double("http_client", :post => double("response", {:code => 200, :body => '{ "score": 100, "uuid": "uuid", "action": "b", "status": 0 }'}))
+    validator = PxModule::PerimeterxS2SValidator.new(@config, http_client)
+    expect(px_ctx.context[:block_action]).to be_nil
+
+    px_ctx = validator.verify(px_ctx)
+    expect(px_ctx.context[:block_action]).to eq "block"
+    expect(px_ctx.context[:blocking_reason]).to eq "s2s_high_score"
+  end
+
+  it "ctx will contain block_action from block type and blocking_reason should be empty" do
+    px_ctx = PxModule::PerimeterXContext.new(@config, @req)
+
+    http_client = double("http_client", :post => double("response", {:code => 200, :body => '{"score": 0, "uuid": "uuid", "action": "b", "status": 0}'}))
+    validator = PxModule::PerimeterxS2SValidator.new(@config, http_client)
+    expect(px_ctx.context[:block_action]).to be_nil
+
+    px_ctx = validator.verify(px_ctx)
+    expect(px_ctx.context[:block_action]).to eq "block"
+    expect(px_ctx.context[:blocking_reason]).to be_nil
+  end
+
+  it "ctx will contain block_action from challenge type and have block block_action_data" do
+    px_ctx = PxModule::PerimeterXContext.new(@config, @req)
+
+    http_client = double("http_client", :post => double("response", {:code => 200 ,:body  => '{"score": 0, "uuid": "uuid", "action": "j", "status": 0, "action_data": {"body": "challenge_body"}}'}))
+    validator = PxModule::PerimeterxS2SValidator.new(@config, http_client)
+    expect(px_ctx.context[:block_action]).to be_nil
+
+    px_ctx = validator.verify(px_ctx)
+    expect(px_ctx.context[:block_action]).to eq 'challenge'
+    expect(px_ctx.context[:block_action_data]).not_to be_nil
+  end
+
+end
